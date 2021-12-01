d5661d8c81ca216851d86971b9e1d45802febff3
dangloc <dangloc522@gmail.gmailcom>
2021-12-01 23:01:29 +0700

    add Identity

--- Models/MyWebContext.cs
+++ Models/MyWebContext.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace razorwebapp.models
 {
-    public class MyWebContext : DbContext
+    public class MyWebContext : IdentityDbContext<AppUser>
     {
         public MyWebContext(DbContextOptions<MyWebContext> options) : base(options)
         {
@@ -14,6 +15,16 @@ namespace razorwebapp.models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                 var tablename = entity.GetTableName();
+                 if(tablename.StartsWith("AspNet"))
+                 {
+                     entity.SetTableName(tablename.Substring(6));
+                 }
+
+            }
         }
 
         public DbSet<Article> articles { get; set; }
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Album.Mail;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,10 +29,50 @@ namespace razorwebapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            var mailsetting = Configuration.GetSection("MailSettings");
+            services.Configure<MailSettings>(mailsetting);
+            services.AddSingleton<IEmailSender, SendMailService>();
+
             services.AddRazorPages();
             services.AddDbContext<MyWebContext>(options => {
                 string connectionString = Configuration.GetConnectionString("MyWebContext");
                 options.UseSqlServer(connectionString);
+            });
+
+            // dang ki Identity
+             services.AddIdentity<AppUser, IdentityRole>()
+             .AddEntityFrameworkStores<MyWebContext>()
+             .AddDefaultTokenProviders();
+
+            // services.AddDefaultIdentity<AppUser>()
+            // .AddEntityFrameworkStores<MyWebContext>()
+            // .AddDefaultTokenProviders();
+
+            // Truy cập IdentityOptions
+            services.Configure<IdentityOptions> (options => {
+                // Thiết lập về Password
+                options.Password.RequireDigit = false; // Không bắt phải có số
+                options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+                options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+                options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+                options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+                options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+                // Cấu hình Lockout - khóa user
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes (5); // Khóa 5 phút
+                options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Cấu hình về User.
+                options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+                // Cấu hình đăng nhập.
+                options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+                options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+
             });
         }
 
@@ -51,12 +95,25 @@ namespace razorwebapp
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
             });
+
         }
     }
 }
+
+/* 
+Identity:  
+    - Authentication: xac thuc danh tinh => login, logout...
+    -Authorization: xac thuc quyen truy cap
+    -quan li User: Signup, userm, 
+Identity/Account/Login
+Identity/Account/Manage\
+
+dotnet aspnet-codegenerator identity -dc razorwebapp.models.MyWebContext
+*/
